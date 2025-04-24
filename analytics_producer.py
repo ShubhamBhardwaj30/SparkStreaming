91ec718deef8ebc40b5ea2189f3b09122036f6b1
Shubham Bhardwaj <Shubbi.30nov@gmail.com>
2025-04-24 12:40:01 -0400

    data flowing from kafka to spark to db

--- analytics_producer.py
+++ analytics_producer.py
@@ -1,11 +1,17 @@
 from kafka import KafkaProducer, KafkaConsumer
-
+import time
 # Connect to Kafka
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 for i in range(1):
     # Send a message to the 'analytics' topic
-    producer.send('analytics', "Hello Kafka, This is working".format(i).encode('utf-8'))
+    current_time = time.time()
+    local_time = time.localtime(current_time)
+
+    hour = local_time.tm_hour
+    minute = local_time.tm_min
+    second = local_time.tm_sec
+    producer.send('analytics', "Hello Kafka, This is working, The time is {}:{}:{} ".format(hour, minute, second).encode('utf-8'))
 
 # Flush the producer to ensure all messages are sent
 producer.flush()
